7dfc90b641bc66d13223cd9e7a123e8a9fe66636
Elliot Hatch <elliot.hatch@gmail.com>
2015-04-06 12:29:56 +0000

    Added workingDir flag to forever start script

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/local/bin/forever -a -l forever.log -o out.log -e err.log start /srv/nodeserver/mazenet/server.js
+/usr/local/bin/forever -a -l forever.log -o out.log -e err.log --workingDir /srv/nodeserver/mazenet start /srv/nodeserver/mazenet/server.js
