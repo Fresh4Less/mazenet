5198f64c45f81c1ff9ca86f998758de3cf020930
Samuel Davidson <samdamana@gmail.com>
2016-03-10 16:50:52 -0700

    Added more tests.

--- client_tests/services/ClickNetService.spec.ts
+++ client_tests/services/ClickNetService.spec.ts
@@ -18,6 +18,30 @@ describe('Unit: UserService', ()=> {
     });
     it('should be defined', ()=>{
         expect(ClickNetService).toBeDefined();
+        expect(ClickNetService.AwaitingClick).toBeFalsy();
+    });
+    it('should return a promise after a click request', ()=> {
+        var promise:ng.IPromise<MouseEvent> = ClickNetService.RequestClick();
+        expect(promise).toBeDefined();
+    });
+    it('should return the same promise with multiple requests', ()=> {
+        var promise1:ng.IPromise<MouseEvent> = ClickNetService.RequestClick();
+        var promise2:ng.IPromise<MouseEvent> = ClickNetService.RequestClick();
+        expect(promise1).toEqual(promise2);
+    });
+    it('should set awaiting click to true after a click is requested', ()=> {
+       expect(ClickNetService.AwaitingClick).toBeFalsy();
+        ClickNetService.RequestClick();
+        expect(ClickNetService.AwaitingClick).toBeTruthy();
+    });
+    it('should resolve the promise with a mouseEvent', (done)=> {
+        var promise:ng.IPromise<MouseEvent> = ClickNetService.RequestClick();
+        var mouseEvent:MouseEvent = new MouseEvent;
+        promise.then((event)=>{
+            expect(event).toEqual(mouseEvent);
+            done();
+        })
+        ClickNetService.ResolveClick(mouseEvent);
     });
 
 });
