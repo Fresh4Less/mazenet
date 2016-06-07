6bde3975e6e0b406cbc8ceefed9a0a60134fe3ce
Elliot Hatch <elliot.hatch@gmail.com>
2016-06-06 23:01:37 -0700

    Fixed element create issue

--- server/elements/sockets.js
+++ server/elements/sockets.js
@@ -18,37 +18,32 @@ function initialze(route, options) {
 
 	router.get('/create', function(req, res, next) {
 		// find the room the user is currently in and create the element there
-		var room = req.socket.rooms.find(function(r) {
-			return r.indexOf(opts.pageRoomPrefix) === 0;
-		});
+		if(req.socket.mazenet.room === undefined) {
+			var err = new CustomErrors.UserNotOnPageError('Not on a page. Enter a page to create an element on it.');
+			err.status = 409;
+			return next(err);
+		}
 
 		if(req.body !== undefined) {
 			req.body.creator = req.socket.mazenet.uId;
 		}
 
-		if(room !== undefined) {
-			elementsService.createElement(room.substring((opts.pageRoomPrefix + '/').length), req.body)
-				.then(function(element) {
-					req.socket.broadcast.to(room).emit(req.baseUrl + '/created', element);
-					res.status(201).send(element);
-				})
-				.catch(ValidatorErrors.ValidationError, function(err) {
-					err.status = 400;
-					next(err);
-				})
-				.catch(CustomErrors.NotFoundError, function(err) {
-					err.status = 404;
-					next(err);
-				})
-				.catch(function(err) {
-					next(err);
-				});
-		}
-		else {
-			var err = new CustomErrors.UserNotOnPageError('Not on a page. Enter a page to create an element on it.');
-			err.status = 409;
+		elementsService.createElement(req.socket.mazenet.room.substring((opts.pageRoomPrefix + '/').length), req.body)
+		.then(function(element) {
+			req.socket.broadcast.to(req.socket.mazenet.room).emit(req.baseUrl + '/created', element);
+			res.status(201).send(element);
+		})
+		.catch(ValidatorErrors.ValidationError, function(err) {
+			err.status = 400;
 			next(err);
-		}
+		})
+		.catch(CustomErrors.NotFoundError, function(err) {
+			err.status = 404;
+			next(err);
+		})
+		.catch(function(err) {
+			next(err);
+		});
 	});
 
 	function middleware(socket, next) {
